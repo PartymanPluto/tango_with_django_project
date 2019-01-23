8ff5b9062df882e97c4cc418a550afe0646a3711
Daniel McQuaid <2310911m@student.gla.ac.uk>
2019-01-23 18:11:27 +0000

    Chapter 3 exercises(2)

--- rango/urls.py
+++ rango/urls.py
@@ -4,9 +4,9 @@ Created on Mon Jan 21 16:39:37 2019
 
 @author: thero
 """
-Created on Mon Jan 21 16:39:37 2019
+"Created on Mon Jan 21 16:39:37 2019"
 
-@author: thero
+"@author: thero"
 
 from django.conf.urls import url
 from rango import views
